f1263e01eea6b6015a62372c355d3a2f7600bdb8
Juan Dorado <jfdoradotr@icloud.com>
2024-12-26 12:29:52 -0600

    Migrate `User` and `Friend` models to SwiftData models

--- FriendsFace/FriendsFace/Friend.swift
+++ FriendsFace/FriendsFace/Friend.swift
@@ -3,8 +3,25 @@
 //
 
 import Foundation
+import SwiftData
 
-struct Friend: Hashable, Decodable {
-  let id: UUID
-  let name: String
+@Model
+class Friend: Hashable, Decodable {
+  var id: UUID
+  var name: String
+
+  init(id: UUID, name: String) {
+    self.id = id
+    self.name = name
+  }
+
+  enum CodingKeys: CodingKey {
+    case id, name
+  }
+
+  required init(from decoder: any Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+    self.id = try container.decode(UUID.self, forKey: .id)
+    self.name = try container.decode(String.self, forKey: .name)
+  }
 }
